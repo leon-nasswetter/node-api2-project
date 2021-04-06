d000188a9c20bd70aefe0ab49f2f807f48f8fdaa
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-06 13:15:11 -0700

    mvp

--- api/server.js
+++ api/server.js
@@ -2,8 +2,10 @@
 // require your posts router and connect it here
 const express = require("express")
 const server = express()
+const postRouter = require("./posts/posts-router")
 
 server.use(express.json())
+server.use("/api/posts", postRouter)
 
 server.get("/", (req, res) => {
     res.status(200).json("it works")
