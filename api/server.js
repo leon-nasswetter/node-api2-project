fb26082858379eea2f3b805a6df1bc57e9ed5197
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-06 11:47:18 -0700

    initial server setup

--- api/server.js
+++ api/server.js
@@ -1,2 +1,12 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require("express")
+const server = express()
+
+server.use(express.json())
+
+server.get("/", (req, res) => {
+    res.status(200).json("it works")
+})
+
+module.exports = server
